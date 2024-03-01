8311ea37aba62394ad5b91c76042851c4278ef03
Slamm Khelmer A. Luab <slammkhelmer@gmail.com>
2024-03-01 13:26:05 +0800

    Cause I think we'd make a good team

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ io.on('connection', (socket) => {
     
     socket.on('chat message', (msg) => {
         console.log("Message: " + msg);
+        io.emit('chat message', msg);
     })
     
     socket.on('disconnect', () => {
